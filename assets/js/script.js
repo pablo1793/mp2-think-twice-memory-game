c967111c77ce5ba5ce2d65f09bf8dd36b76d4216
pablo1793 <pablopagework@gmail.com>
2021-10-21 16:27:45 +0000

    feat: Add js script to html and begin adding functionality in script.js file, maint: Remove unused fonts.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('')
+    }
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            // game.startGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // game.flipCard(card);
+        });
+    });
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
