e1d3a2fbfb603bb2db6cb6cdb86f4a9b551c6d19
pablo1793 <pablopagework@gmail.com>
2021-10-23 12:45:44 +0000

    feat: Code startGame and flipCard functions + constructor for ThinkTwice class in script.js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,21 +31,53 @@ class AudioController {
     }
 }
 
+class ThinkTwice {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card) {
+        if (this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
+        }
+    }
+
+    canFlipCard(card) {
+        return true;
+        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+}
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new ThinkTwice(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startGame();
+            game.startGame();
             let audioController = new AudioController();
             audioController.startMusic();
         });
     });
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            // game.flipCard(card);
+            game.flipCard(card);
         });
     });
 }
