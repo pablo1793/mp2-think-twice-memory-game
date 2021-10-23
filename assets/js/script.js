e499d4c6cc85a3097256ab3f503c6c899fa70eb5
pablo1793 <pablopagework@gmail.com>
2021-10-23 16:15:20 +0000

    feat: Code various cardMatch functions for ThinkTwice class.

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ class AudioController {
     }
     victory() {
         this.stopMusic();
-        this.victory.play();
+        this.victorySound.play();
     }
     gameOver() {
         this.stopMusic();
@@ -69,8 +69,39 @@ class ThinkTwice {
             this.ticker.innerText = this.totalClicks;
             card.classList.add('visible');
 
-            //if statement
+            if (this.cardToCheck)
+                this.checkForCardMatch(card);
+            else
+                this.cardToCheck = card;
         }
+    }
+    checkForCardMatch(card) {
+        if (this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMisMatch(card, this.cardToCheck);
+
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        this.audioController.match();
+        if (this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }
+    cardMisMatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+    getCardType(card) {
+        return card.getElementsByClassName('instrument')[0].src;
     }
     startCountDown() {
         return setInterval(() => {
@@ -100,8 +131,7 @@ class ThinkTwice {
     }
 
     canFlipCard(card) {
-        return true;
-        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
 }
 
